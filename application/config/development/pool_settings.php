1eb771a7204fed9539d690db5248f08f3c9075b6
John Schop <john.schop@gmail.com>
2017-04-03 10:06:58 -0400

    test

--- application/config/development/pool_settings.php
+++ application/config/development/pool_settings.php
@@ -1,7 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/* March 2017 */
+
 $config['pool_title'] = "Worldcup 2018";
 $config['pool_dev'] = "John Schop";
 $config['pool_version'] = "1.0 Alpha";
 $config['pool_default_language'] = "english";
+$config['testing'] = TRUE;
